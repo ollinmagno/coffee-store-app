0166d0aa890c2ae966cfeeb3f8d974c825fa5ed7
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-24 13:56:33 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:coffeebreak/src/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Coffee',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
