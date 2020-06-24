936795a0690c0baf408b75b0b72bbb736befe2d3
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-24 20:18:49 -0300

    favoritos

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -4,8 +4,30 @@ import 'package:flutter/material.dart';
 class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('User user'),
+              accountEmail: Text('user@email.com.br'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(''),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.star, color: Colors.amber.shade600,),
+              title: Text('Favoritos'),
+              subtitle: Text('favoritos'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                print('Item 1');
+                Navigator.pop(context);
+              }
+            )
+          ],
+        ),
+      ),
     );
   }
 }
