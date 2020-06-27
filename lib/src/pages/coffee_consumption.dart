ee4b379c8f527a0fc5d5e32e85832855d9c585bc
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-06-27 15:56:08 -0300

    maps e consumption

---
+++ lib/src/pages/coffee_consumption.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CoffeeConsumption extends StatefulWidget {
+  @override
+  _CoffeeConsumptionState createState() => _CoffeeConsumptionState();
+}
+
+class _CoffeeConsumptionState extends State<CoffeeConsumption> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/pages/find_on_map.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FindOnMap extends StatefulWidget {
+  @override
+  _FindOnMapState createState() => _FindOnMapState();
+}
+
+class _FindOnMapState extends State<FindOnMap> {
+  _body() {
+    return Container();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber.shade600,
+        title: Text('Coffee Store'),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+}
